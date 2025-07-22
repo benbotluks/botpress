e15b53e421e43e72b019ea58d4df8dec10344212
Ben Luks <benjamin.luks@botpress.com>
2025-07-22 15:26:50 -0400

    ping event

--- integrations/gmail/definitions/events.ts
+++ integrations/gmail/definitions/events.ts
@@ -1,3 +1,14 @@
 import * as sdk from '@botpress/sdk'
+import { z } from '@botpress/sdk'
 
-export const events = {} as const satisfies sdk.IntegrationDefinitionProps['events']
+export const ping = {
+    title: "ping",
+    description: "a test event",
+    schema: z.object({
+        description: z.string().title("description").describe("Just a test event")
+    })
+}
+
+export const events = {
+    ping
+} as const satisfies sdk.IntegrationDefinitionProps['events']
